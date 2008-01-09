6d03bebe56b2f8a57120bd8f66e66ed964afd0b2
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2008-01-09 13:46:05 +0000

    Added Framework classifier to setup.py
    git-svn-id: https://django-voting.googlecode.com/svn/trunk@61 662f01ad-f42a-0410-a340-718c64ddaef4

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     packages = ['voting', 'voting.templatetags', 'voting.tests'],
     classifiers = ['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
+                   'Framework :: Django',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
