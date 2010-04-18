d4e835201493766ce01a5e62398988b0b400d755
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2010-04-19 04:23:52 +0800

    regex supporting dots in app_label (namespaces)

--- voting/urls.py
+++ voting/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
-    url(r"^vote/(?P<app_label>[\w-]+)/(?P<model_name>[\w-]+)/"\
+    url(r"^vote/(?P<app_label>[\w-\.]+)/(?P<model_name>\w+)/"\
         "(?P<object_id>\d+)/(?P<direction>up|down|clear)/$", 
         "voting.views.vote_on_object_with_lazy_model", { 
-            'allow_xmlhttprequest': True, 
+            "allow_xmlhttprequest": True, 
         },
         name="voting_vote"
     ),
