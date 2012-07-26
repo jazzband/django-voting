9e2d62a876456b15614b0e324c75b8d55bd482a3
Jannis Leidel <jannis@leidel.info>
2012-07-26 14:42:39 +0200

    Updated license, changelog and version.

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
 
 # Dynamically calculate the version based on tagging.VERSION.
 version_tuple = __import__('voting').VERSION
@@ -15,19 +8,28 @@ else:
     version = "%d.%d" % version_tuple[:2]
 
 setup(
-    name = 'django-voting',
-    version = version,
-    description = 'Generic voting application for Django',
-    author = 'Jonathan Buchanan',
-    author_email = 'jonathan.buchanan@gmail.com',
-    url = 'http://code.google.com/p/django-voting/',
-    packages = ['voting', 'voting.templatetags', 'voting.migrations', 'voting.tests'],
-    classifiers = ['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
+    name='django-voting',
+    version=version,
+    description='Generic voting application for Django',
+    author='Jonathan Buchanan',
+    author_email='jonathan.buchanan@gmail.com',
+    maintainer='Jannis Leidel',
+    maintainer_email='jannis@leidel.info',
+    url='http://github.com/jezdez/django-voting/',
+    packages=[
+        'voting',
+        'voting.migrations',
+        'voting.templatetags',
+        'voting.tests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
 )
