f6aace7d236309d2d80ef95c126696f6314d6048
kaleissin <kaleissin@gmail.com>
2013-12-05 22:46:32 +0100

    Use aggregate() and annotate() instead of raw sql

--- voting/managers.py
+++ voting/managers.py
@@ -1,39 +1,9 @@
 from django.conf import settings
-from django.db import connection, models
-
-try:
-    from django.db.models.sql.aggregates import Aggregate
-except ImportError:
-    supports_aggregates = False
-else:
-    supports_aggregates = True
-
+from django.db import models
+from django.db.models import Sum, Count
 from django.contrib.contenttypes.models import ContentType
 
 ZERO_VOTES_ALLOWED = getattr(settings, 'VOTING_ZERO_VOTES_ALLOWED', False)
-
-if supports_aggregates:
-    class CoalesceWrapper(Aggregate):
-        sql_template = 'COALESCE(%(function)s(%(field)s), %(default)s)'
-
-        def __init__(self, lookup, **extra):
-            self.lookup = lookup
-            self.extra = extra
-
-        def _default_alias(self):
-            return '%s__%s' % (self.lookup, self.__class__.__name__.lower())
-        default_alias = property(_default_alias)
-
-        def add_to_query(self, query, alias, col, source, is_summary):
-            super(CoalesceWrapper, self).__init__(col, source, is_summary, **self.extra)
-            query.aggregate_select[alias] = self
-
-    class CoalesceSum(CoalesceWrapper):
-        sql_function = 'SUM'
-
-    class CoalesceCount(CoalesceWrapper):
-        sql_function = 'COUNT'
-
 
 class VoteManager(models.Manager):
     def get_score(self, obj):
@@ -42,17 +12,13 @@ class VoteManager(models.Manager):
         the number of votes it's received.
         """
         ctype = ContentType.objects.get_for_model(obj)
-        result = self.filter(object_id=obj._get_pk_val(),
-                             content_type=ctype).extra(
-            select={
-                'score': 'COALESCE(SUM(vote), 0)',
-                'num_votes': 'COALESCE(COUNT(vote), 0)',
-        }).values_list('score', 'num_votes')[0]
+        result = (self.filter(object_id=obj._get_pk_val(), content_type=ctype)
+            .aggregate(score=Sum('vote'), num_votes=Count('vote'))
+        )
 
-        return {
-            'score': int(result[0]),
-            'num_votes': int(result[1]),
-        }
+        if result['score'] is None:
+            result['score'] = 0
+        return result
 
     def get_scores_in_bulk(self, objects):
         """
@@ -65,27 +31,15 @@ class VoteManager(models.Manager):
 
         ctype = ContentType.objects.get_for_model(objects[0])
 
-        if supports_aggregates:
-            queryset = self.filter(
-                object_id__in=object_ids,
-                content_type=ctype,
-            ).values(
-                'object_id',
-            ).annotate(
-                score=CoalesceSum('vote', default='0'),
-                num_votes=CoalesceCount('vote', default='0'),
-            )
-        else:
-            queryset = self.filter(
-                object_id__in=object_ids,
-                content_type=ctype,
-                ).extra(
-                    select={
-                        'score': 'COALESCE(SUM(vote), 0)',
-                        'num_votes': 'COALESCE(COUNT(vote), 0)',
-                    }
-                ).values('object_id', 'score', 'num_votes')
-            queryset.query.group_by.append('object_id')
+        queryset = self.filter(
+            object_id__in=object_ids,
+            content_type=ctype,
+        ).values(
+            'object_id',
+        ).annotate(
+            score=Sum('vote'), 
+            num_votes=Count('vote')
+        )
 
         vote_dict = {}
         for row in queryset:
@@ -120,47 +74,28 @@ class VoteManager(models.Manager):
             self.create(user=user, content_type=ctype,
                         object_id=obj._get_pk_val(), vote=vote)
 
-    def get_top(self, Model, limit=10, reversed=False):
+    def get_top(self, model, limit=10, reversed=False):
         """
         Get the top N scored objects for a given model.
 
         Yields (object, score) tuples.
         """
-        ctype = ContentType.objects.get_for_model(Model)
-        query = """
-        SELECT object_id, SUM(vote) as %s
-        FROM %s
-        WHERE content_type_id = %%s
-        GROUP BY object_id""" % (
-            connection.ops.quote_name('score'),
-            connection.ops.quote_name(self.model._meta.db_table),
-        )
-
-        # MySQL has issues with re-using the aggregate function in the
-        # HAVING clause, so we alias the score and use this alias for
-        # its benefit.
-        if settings.DATABASE_ENGINE == 'mysql':
-            having_score = connection.ops.quote_name('score')
-        else:
-            having_score = 'SUM(vote)'
+        ctype = ContentType.objects.get_for_model(model)
+        results = self.filter(content_type=ctype).values('object_id').annotate(score=Sum('vote'))
         if reversed:
-            having_sql = ' HAVING %(having_score)s < 0 ORDER BY %(having_score)s ASC LIMIT %%s'
+            results = results.order_by('score')
         else:
-            having_sql = ' HAVING %(having_score)s > 0 ORDER BY %(having_score)s DESC LIMIT %%s'
-        query += having_sql % {
-            'having_score': having_score,
-        }
-
-        cursor = connection.cursor()
-        cursor.execute(query, [ctype.id, limit])
-        results = cursor.fetchall()
+            results = results.order_by('-score')
 
         # Use in_bulk() to avoid O(limit) db hits.
-        objects = Model.objects.in_bulk([id for id, score in results])
+        objects = model.objects.in_bulk([item['object_id'] for item in results[:limit]])
 
         # Yield each object, score pair. Because of the lazy nature of generic
         # relations, missing objects are silently ignored.
-        for id, score in results:
+        for item in results[:limit]:
+            id, score = item['object_id'], item['score']
+            if not score:
+                continue
             if id in objects:
                 yield objects[id], int(score)
 
