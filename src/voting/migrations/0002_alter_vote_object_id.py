c1192108876cad2c82f7c269fcf7526062b53ca7
Hasan Ramezani <hasan.r67@gmail.com>
2022-05-27 14:31:39 +0200

    Lint the code with black and isort

--- src/voting/migrations/0002_alter_vote_object_id.py
+++ src/voting/migrations/0002_alter_vote_object_id.py
@@ -4,13 +4,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('voting', '0001_initial'),
+        ("voting", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='vote',
-            name='object_id',
+            model_name="vote",
+            name="object_id",
             field=models.TextField(),
         ),
     ]
